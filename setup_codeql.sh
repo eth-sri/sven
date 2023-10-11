5d6bf70fc1af6207d1682985b28af997d5c0f542
Jingxuan He <LostBenjamin@users.noreply.github.com>
2023-10-11 15:21:58 +0200

    Update setup_codeql.sh

--- setup_codeql.sh
+++ setup_codeql.sh
@@ -1,5 +1,5 @@
 wget https://github.com/github/codeql-cli-binaries/releases/download/v2.11.1/codeql-linux64.zip
 unzip codeql-linux64.zip
 git clone --depth=1 --branch codeql-cli-2.11.1 https://github.com/github/codeql.git codeql/codeql-repo
-codeql/codeql pack download codeql-cpp codeql-python@0.6.2 codeql/ssa codeql/tutorial codeql/regex codeql/util
+codeql/codeql pack download codeql-cpp@0.7.1 codeql-python@0.6.2 codeql/ssa@0.0.16 codeql/tutorial@0.0.9 codeql/regex@0.0.12 codeql/util@0.0.9
 cp data_eval/trained/cwe-190/1-c/ArithmeticTainted.ql codeql/codeql-repo/cpp/ql/src/Security/CWE/CWE-190/ArithmeticTainted.ql
